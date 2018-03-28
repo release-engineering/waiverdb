16a023c434ee8606c752e100b0bd9704c08bd5fe
Dan Callaghan <dcallagh@redhat.com>
2018-03-28 12:26:12 +1000

    use a more compact log format

--- waiverdb/logger.py
+++ waiverdb/logger.py
@@ -5,8 +5,7 @@ import sys
 
 
 def log_to_stdout(app, level=logging.INFO):
-    fmt = '[%(filename)s:%(lineno)d] ' if app.debug else '%(module)-12s '
-    fmt += '%(asctime)s %(levelname)-7s %(message)s'
+    fmt = '%(asctime)s [pid %(process)5d] %(name)s %(levelname)s %(message)s'
     datefmt = '%Y-%m-%d %H:%M:%S'
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setLevel(level)
