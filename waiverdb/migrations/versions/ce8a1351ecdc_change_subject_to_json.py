518ef8d338048a581feb4fd2be5c22eec435d10a
Lukas Holecek <hluk@email.cz>
2023-06-06 14:45:05 +0200

    Fix DB migrations

--- waiverdb/migrations/versions/ce8a1351ecdc_change_subject_to_json.py
+++ waiverdb/migrations/versions/ce8a1351ecdc_change_subject_to_json.py
@@ -15,16 +15,15 @@ from alembic import op
 from sqlalchemy import Text, text
 from sqlalchemy.dialects.postgresql import JSON
 from waiverdb.models.base import json_serializer
-from waiverdb.models import db
 
 
 def upgrade():
     # Re-serialize all subject values to ensure they match the new, consistent
     # serialization we are using.
     connection = op.get_bind()
-    for row in db.session.execute(text('SELECT id, subject FROM waiver')):
+    for row in connection.execute(text('SELECT id, subject FROM waiver')):
         fixed_subject = json_serializer(json.loads(row['subject']))
-        db.session.execute(text('UPDATE waiver SET subject = :subject WHERE id = :id'),
+        connection.execute(text('UPDATE waiver SET subject = :subject WHERE id = :id'),
                            subject=fixed_subject, id=row['id'])
 
     op.drop_index('ix_waiver_subject')
