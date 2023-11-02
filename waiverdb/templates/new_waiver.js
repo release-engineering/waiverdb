a956076fbee6f570ff45589a1c6e2f2454215936
Valerij Maljulin <vmaljuli@redhat.com>
2023-11-02 15:25:04 +0100

    Moving js to a separate file, refactoring templates to use a common layout

---
+++ waiverdb/templates/new_waiver.js
@@ -0,0 +1,38 @@
+const WAIVERS_URL = "{{ url_for('api_v1.waivers_resource') }}";
+
+$(document).ready(function () {
+    $("form").on("submit", function(ev) {
+        ev.preventDefault();
+        const testcaseName = $("#testcase").val();
+        $.ajax({
+            url: WAIVERS_URL,
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                subject_type: $("#subject_type").val(),
+                subject_identifier: $("#subject_identifier").val(),
+                testcase: testcaseName,
+                product_version: $("#product_version").val(),
+                comment: $("#comment").val(),
+                scenario: $("#scenario").val()
+            }),
+            success: function (data, status, jqXHR) {
+                $("#result-text-error").addClass("d-none");
+                $("#result-text-success").removeClass("d-none");
+                $("#result-link").attr("href", `${WAIVERS_URL}${data.id}`);
+                $("#new-waiver-id").text(data.id);
+                $("#waiver-result").removeClass("alert-danger d-none").addClass("alert-success");
+            },
+            error: function (jqXHR, textStatus) {
+                $("#result-text-success").addClass("d-none");
+                $("#result-text-error").removeClass("d-none");
+                let addition = "";
+                if(jqXHR.status === 401) {
+                    addition = `<a href="{{ url_for('api_v1.permissions_resource', html='1')}}&testcase=${encodeURIComponent(testcaseName)}" target="_blank">See who has permissions to waive ${testcaseName}</a>`;
+                }
+                $("#error-desc").html(`Creation failed, status: ${jqXHR.status} ${jqXHR.statusText} ${addition}`);
+                $("#waiver-result").removeClass("alert-success d-none").addClass("alert-danger");
+            }
+        });
+    });
+});
