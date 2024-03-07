0926036c079c261489c5923ebba1524115d2f526
Lukas Holecek <hluk@email.cz>
2024-03-07 06:54:03 +0100

    Fix version in /api/v1.0/about

--- waiverdb/__init__.py
+++ waiverdb/__init__.py
@@ -1,2 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0+
-__version__ = '1.4.0'
+import importlib.metadata as importlib_metadata
+
+try:
+    __version__ = importlib_metadata.version(__name__)
+except importlib_metadata.PackageNotFoundError:
+    # If the app is not installed but run from git repository clone, get the
+    # version from pyproject.toml.
+    try:
+        import tomllib
+    except ImportError:
+        import toml as tomllib  # type: ignore
+
+    with open("pyproject.toml", "r") as f:
+        pyproject = tomllib.load(f)  # type: ignore
+
+    __version__ = pyproject["tool"]["poetry"]["version"]
