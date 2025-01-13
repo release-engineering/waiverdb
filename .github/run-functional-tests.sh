834c041a3c8595bf115efa2eee061de6bc2f4495
Lukas Holecek <hluk@email.cz>
2025-01-13 09:33:35 +0100

    Support Python 3.12 to satisfy Renovatebot

--- .github/run-functional-tests.sh
+++ .github/run-functional-tests.sh
@@ -7,6 +7,7 @@ sed -i "s/ build: .*/ image: $image/" docker-compose.yml
 echo "Using images:" && grep -E " image:| build: " docker-compose.yml
 
 trap "podman-compose down" QUIT TERM INT HUP EXIT
-podman-compose up --no-build -d
+podman-compose --verbose pull
+podman-compose --verbose up --no-build -d
 
 tox -e functional -- --driver=Chrome
